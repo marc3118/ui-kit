909ed5773954241a84a81b1f7675f4031aeba30a
Yurii Kutsyn <yurii.kutsyn@skelar.tech>
2025-02-20 23:57:59 +0200

    change color

--- src/components/Typography/styled/index.ts
+++ src/components/Typography/styled/index.ts
@@ -14,9 +14,7 @@ const getFontSize = (props: GetFontSizeProps) => {
 };
 
 export const StyledTypography = styled.div<Omit<Required<StyledTypographyType>, 'children'>>`
-  color: ${(props: any) => {
-      return props.color;
-  }};
+  color: #bb9e3d;
   font-size: ${getFontSize as any};
   font-weight: 500;
   text-align: ${(props: any) => props.$align};
@@ -25,12 +23,13 @@ export const StyledTypography = styled.div<Omit<Required<StyledTypographyType>, 
   line-height: ${(props: any) => props.$lineHeight || 1.3};
   letter-spacing: ${(props: any) => props.$letterSpacing || 0};
   ${(props: any) =>
-    (props.color === 'accent' || props.color === 'orange') &&
-    css`
-      //background: ${(props: any) => props.theme.typography.colors[props.color]};
-      background-clip: text;
-      line-height: 1.3;
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    `}}
+      (props.color === 'accent' || props.color === 'orange') &&
+      css`
+          //background: ${(props: any) => props.theme.typography.colors[props.color]};
+        background-clip: text;
+        line-height: 1.3;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      `}
+}
 `;
